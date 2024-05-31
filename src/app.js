9206cb5a909e02d874c74bdac8a4a86b5d654308
gelbwong <karinawong04@gmail.com>
2024-05-31 20:06:12 +1000

    connect project to api
    -allows themes to be submitted then pushed to api
    -generates a response the replaces a html element

--- src/app.js
+++ src/app.js
@@ -5,21 +5,34 @@ ai-response
 */
 function runApi(event) {
   event.preventDefault();
-  console.log(event);
-  /*
+  let searchFormElement = document.querySelector("#search-input");
 
-  userInput = event;
+  userInput = searchFormElement.value;
   key = "a5ca0d6dt74bbobcf0c9aa390574f791";
-  prompt = `Can you find me a book with the topics of ${userInput}`;
+  let prompt = `Can you find me a book with the topics of ${userInput}`;
   context =
     "You are a library AI assistant, that enjoys to give readers with unique reading suggestions with conceise reasoning";
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
-  axios.get(apiUrl).then(displayResponse);*/
+  new Typewriter("#ai-feedback", {
+    strings: "Please be patient while the joke is being generated ❤️",
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+    loop: true,
+  });
+
+  axios.get(apiUrl).then(displayResponse);
 }
 
-function displayResponse(data) {
-  console.log(data);
+function displayResponse(response) {
+  new Typewriter("#ai-feedback", {
+    strings: `${response.data.answer}`,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+    loop: false,
+  });
 }
 
 let searchFromElement = document.querySelector("#search-form");
